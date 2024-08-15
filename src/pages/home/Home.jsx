75a51fb2bf6b3f38721c8a0cdbe05c566be702ea
AzizbekEngineer <azizbekengineer@gmail.com>
2024-08-15 17:49:22 +0500

    add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Hero from "../../components/hero/Hero";
+import Partner from "../../components/partner/Partner";
+import NewProducts from "../../components/new-products/NewProducts";
+import TopProducts from "../../components/top-products/TopProducts";
+import Browse from "../../components/browse/Browse";
 import SwiperComp from "../../components/swiper/Swiper";
 
 const Home = () => {
   return (
-    <div className="">
+    <>
+      <Hero />
+      <Partner />
+      <NewProducts />
+      <TopProducts />
+      <Browse />
       <SwiperComp />
-    </div>
+    </>
   );
 };
 
