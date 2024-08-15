8f3db65e4cc787036b6607dd07fdca6ff13c43bf
AzizbekEngineer <azizbekengineer@gmail.com>
2024-08-15 17:28:59 +0500

    add

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SwiperComp from "../../components/swiper/Swiper";
+
+const Home = () => {
+  return (
+    <div className="">
+      <SwiperComp />
+    </div>
+  );
+};
+
+export default Home;
